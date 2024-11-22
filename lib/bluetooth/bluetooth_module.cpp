75eb7d04133d394be8fc660c6d189a8c0a8a046e
jotades <giovanni.tauro.ux@gmail.com>
2024-11-22 17:37:02 +0100

    Test bluetooth comunicationimplementation JSON format

--- lib/bluetooth/bluetooth_module.cpp
+++ lib/bluetooth/bluetooth_module.cpp
@@ -4,40 +4,46 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-#include <Arduino.h>
+#include <ArduinoJson.h>
 
 BLECharacteristic *pCharacteristic;
 
 void initializeBluetooth() {
-  BLEDevice::init("jotadesu");
-  BLEServer *pServer = BLEDevice::createServer();
-  
-  // Create a BLE service with a given UUID
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  
-  // Create a BLE characteristic with a given UUID and properties (notify only)
-  pCharacteristic = pService->createCharacteristic(
-                        CHARACTERISTIC_UUID,
-                        BLECharacteristic::PROPERTY_NOTIFY
+    BLEDevice::init("jo");
+    BLEServer *pServer = BLEDevice::createServer();
+    
+    // Create a BLE service with a given UUID
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    
+    // Create a BLE characteristic with a given UUID and properties (notify only)
+    pCharacteristic = pService->createCharacteristic(
+                          CHARACTERISTIC_UUID,
+                          BLECharacteristic::PROPERTY_NOTIFY
                       );
-  
-  // Create a BLE Descriptor
-  pCharacteristic->addDescriptor(new BLE2902());
-  
-  // Start the BLE service
-  pService->start();
-  
-  // Start advertising the BLE service
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pServer->getAdvertising()->start();
-  
-  Serial.println("Waiting for client connection to notify...");
-  delay(1000);  // Allow time for Bluetooth initialization
+    
+    // Create a BLE Descriptor
+    pCharacteristic->addDescriptor(new BLE2902());
+    
+    // Start the BLE service
+    pService->start();
+    
+    // Start advertising the BLE service
+    BLEAdvertising *pAdvertising = pServer->getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pServer->getAdvertising()->start();
+    
+    Serial.println("Waiting for client connection to notify...");
+    delay(1000);  // Allow time for Bluetooth initialization
 }
 
-void updateBluetoothData(const String &data) {
-    pCharacteristic->setValue(data.c_str());
-    Serial.println("Sending notification");
+void updateBluetoothData(const JsonObject &jsonData) {
+    // Serialize JSON object to string
+    String jsonString;
+    serializeJson(jsonData, jsonString);
+    
+    // Set the value and send notification
+    pCharacteristic->setValue(jsonString.c_str());
+    Serial.println("Sending notification:");
+    Serial.println(jsonString);
     pCharacteristic->notify();
 }
--- lib/bluetooth/bluetooth_module.h
+++ lib/bluetooth/bluetooth_module.h
@@ -6,7 +6,7 @@
 #include <BLEServer.h>
 #include <BLE2902.h>
 #include <Arduino.h>
-
+#include <ArduinoJson.h>
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -14,6 +14,6 @@
 extern BLECharacteristic *pCharacteristic;
 
 void initializeBluetooth();
-void updateBluetoothData(const String &data);
+void updateBluetoothData(const JsonObject &jsonData);
 
 #endif
